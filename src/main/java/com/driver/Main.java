95870da06c399bc8755fe26222f6645f9d824099
Shilpi <svvar31@gmail.com>
2023-02-08 00:19:00 +0530

    First Submit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,4 +1,22 @@
 package com.driver;
 
 public class Main {
+    public static class Product {
+        public int product(int x, int y) {
+            return x * y;
+        }
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
+        public double product(double x, double y) {
+            return x * y;
+        }
+    }
+
+    public static void main(String[] args) {
+        Product p = new Product();
+        p.product(100, 200);
+        p.product(100, 200, 300);
+        p.product(80.0, 100.0);
+    }
 }
